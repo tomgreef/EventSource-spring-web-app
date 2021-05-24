59594c5c2fcc7ca092f79429551e1a56ed91082c
Tom van Greevenbroek <tom.v.g@outlook.com>
2021-05-24 12:34:19 +0200

    Initial commit

---
+++ Proyecto/src/main/java/taw/intellij/IntelliJApplication.java
@@ -0,0 +1,13 @@
+package taw.intellij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelliJApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelliJApplication.class, args);
+    }
+
+}
